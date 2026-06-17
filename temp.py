314f132d78b14f15d3f2678ec66f0788f7ddd3c8
harshit12122 <2821463.cse@pietgroup.co.in>
2026-06-17 17:05:17 +0530

    message1

--- temp.py
+++ temp.py
@@ -1,14 +1,7 @@
-# from scipy import stats
-# data = [10, 12, 345, 56, 45, 23, 234, 3]
+import os
+import time
 
-# sd = stats.tstd(data)
-# mean = sum(data)/len(data)
-
-# print(stats.norm.cdf(70, loc = mean, scale = sd))
-# print(sd, mean)
-
-from scipy import stats
-print(stats.norm.ppf(.975))
-
-
+while True:
+    os.system("input tap 500 1800")
+    time.sleep(2)
 
